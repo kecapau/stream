4b829be67c5ac70f58a8805381b59a93e12d191b
Kim <newking5@naver.com>
2024-05-19 12:54:54 +0900

    Add git and main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print('aaaa')
+print('aaaa')
+print('aaaa')
+print('aaaa')
+print('aaaa')
+print('aaaaaaadf')
+
+
+#git config --global user.email "newking5@naver.com"
+#git config --global user.name "Kim"
